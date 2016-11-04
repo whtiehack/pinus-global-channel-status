6831016db7f7a138f61589a23e3d5230c90166ea
460454039@qq.com <namei198>
2016-11-04 18:46:25 +0800

    实现 es6 语法，可以使用 yield 调用

---
+++ Test/config/redisConfig.js
@@ -0,0 +1,13 @@
+/**
+ * Created by frank on 16-11-4.
+ */
+
+module.exports.redisChannel =
+{
+    family      : 4,           // 4 (IPv4) or 6 (IPv6)
+    password : 'gameMirror',
+    options     : {},
+    host        : '192.168.31.233',
+    port        : 6800,
+    db          : 5      // optinal, from 0 to 15 with default redis configure
+};
--- Test/redisManager.js
+++ Test/redisManager.js
@@ -1,3 +1,21 @@
 /**
  * Created by frank on 16-11-3.
  */
+
+const co = require('co');
+const config = require('./config/redisConfig').redisChannel;
+const RedisManager = require('../lib/manager/RedisGlobalChannelManager');
+
+const redisManager = new RedisManager(config);
+
+
+
+function *test()
+{
+    yield redisManager.start();
+    const clean = yield redisManager.clean();
+    const add = yield redisManager.add('scene_123','uuid_123','server1');
+    const members = yield redisManager.getMembersBySid('scene_123','server1');
+    console.info(clean, add, members);
+}
+co(test());

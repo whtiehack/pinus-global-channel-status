4cdd790f93712aae30afaeda059ac24eb14abbd2
smallwhite <whitehack@163.com>
2018-06-30 13:24:33 +0800

    do not compile tests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 'use strict';
 
 
+import {GlobalChannelServiceStatus} from "./lib/service/GlobalChannelServiceStatus";
+export {PinusGlobalChannelStatusOptions} from "./lib/manager/StatusChannelManager";
 
+export {GlobalChannelServiceStatus} from './lib/service/GlobalChannelServiceStatus';
 
+/**
+ * 实现一个基本的插件，插件载入时，会被自动扫描handlerPath和remoterPath指向的目录
+ */
+export class GlobalChannelStatusPlugin  {
+    name = 'GlobalChannelStatusPlugin';
+    components = [GlobalChannelServiceStatus];
+}
+
+export function createGlobalChannelStatusPlugin(){
+	return new GlobalChannelStatusPlugin();
+}
 
 export const PomeloExports =  {
 	components : __dirname + '/lib/components/',
